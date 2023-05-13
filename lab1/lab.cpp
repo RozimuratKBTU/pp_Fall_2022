27d0bab631439082bae31f17426659c25809edba
RozimuratKBTU <122789645+RozimuratKBTU@users.noreply.github.com>
2023-05-13 23:59:31 +0600

    Create lab.cpp
    practice

---
+++ lab1/lab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    for(int i =1; i <=9; i++){
+        for (int j = 1; j < 10; j++)
+        {
+            cout<<i*j<<'\t';
+        }
+        cout<<'\n';
+        
+    }
+    return 0 ;
+}
